88c4f0e18471ab41bac4051b4e65a266bc2b1960
kosba <mohamed.kosba96@gmail.com>
2020-10-20 03:49:06 +0200

    تغير الثيم الرئيسي للبرناج

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
-  // تغير الثيم الرئيسي للبرنامج
   theme: ThemeData.dark(),
   home: QRCode(),
 ));
